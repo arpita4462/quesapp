171f7a1af65452a511db115cb5f737a5ef0fa2b0
arpita4462 <arpitapatel4462@gmail.com>
2017-06-19 16:31:57 +0530

    radiobutton listner

--- app/src/main/java/com/atrio/quesapp/QuestionActivity.java
+++ app/src/main/java/com/atrio/quesapp/QuestionActivity.java
@@ -1,8 +1,11 @@
 package com.atrio.quesapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.atrio.quesapp.model.QuestionModel;
 import com.google.firebase.database.DataSnapshot;
@@ -24,14 +28,20 @@ import com.google.firebase.database.ValueEventListener;
 public class QuestionActivity extends AppCompatActivity implements Animation.AnimationListener {
 
     RadioGroup rg_option;
-    RadioButton rb_opA,rb_opB,rb_opC,rb_opD;
-    Button btn_sub;
+    RadioButton rb_opA,rb_opB,rb_opC,rb_opD,rbselect,rbcorrect;
+    Button btn_sub,bt_done;
     TextView tv_sub,tv_ques;
     Animation animFadein,animMove;
     private DatabaseReference db_ref;
     private FirebaseDatabase db_instance;
     public String tittle,correctAns,selectedAns;
-    int qno=1;
+    int qno=1,correctValue =0,checkedRadioButtonID;
+    double totalques;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Value = "correct_value";
+
+    SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +53,16 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
         rb_opC=(RadioButton) findViewById(R.id.rb_opC);
         rb_opD=(RadioButton) findViewById(R.id.rb_opD);
         btn_sub=(Button) findViewById(R.id.btn_submit);
+        bt_done=(Button) findViewById(R.id.bt_sub);
         rg_option=(RadioGroup) findViewById(R.id.rg_option);
+        bt_done.setVisibility(View.GONE);
+
 
         Intent i =  getIntent();
         tittle = i.getStringExtra("Sub");
         tv_sub.setText(tittle);
         btn_sub.setEnabled(false);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
         // set animation listener
         animFadein = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
@@ -58,56 +72,131 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
 
         //firebase database
         db_instance = FirebaseDatabase.getInstance();
+
         db_ref = db_instance.getReference(tittle);
         getQuestion(qno);
 
-//        Log.i("correctans",""+correctAns);
+        checkedRadioButtonID = rg_option.getCheckedRadioButtonId();
+        Log.i("heckedId22",""+checkedRadioButtonID);
 
-        rg_option.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-                btn_sub.setEnabled(true);
-                btn_sub.setBackgroundResource(R.drawable.ripple_effect);
-                switch (checkedId){
-                    case R.id.rb_opA:
-                        selectedAns = rb_opA.getText().toString();
-                        break;
-                    case R.id.rb_opB:
-                        selectedAns = rb_opB.getText().toString();
-                        break;
-                    case R.id.rb_opC:
-                        selectedAns = rb_opC.getText().toString();
-                        break;
-                    case R.id.rb_opD:
-                        selectedAns = rb_opD.getText().toString();
-                        break;
-                    default:
-                        // Your code
-                        break;
-                }
 
-                Log.i("selectedans",""+selectedAns);
-                Log.i("correctans2",""+correctAns);
+        if (checkedRadioButtonID ==-1){
 
-                if (selectedAns.equals(correctAns)){
+            rg_option.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+
+                @Override
+                public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                    btn_sub.setEnabled(true);
+                    btn_sub.setBackgroundResource(R.drawable.ripple_effect);
+
+                    rbselect = (RadioButton)group.findViewById(checkedId);
+                    rbcorrect= (RadioButton)group.findViewById(checkedId);
+
+                    switch (checkedId) {
+                        case R.id.rb_opA:
+                            // Log.i("selectedans",""+selectedAns);
+                            selectedAns = rb_opA.getText().toString();
+                            break;
+                        case R.id.rb_opB:
+                            selectedAns = rb_opB.getText().toString();
+                            break;
+                        case R.id.rb_opC:
+                            selectedAns = rb_opC.getText().toString();
+                            break;
+                        case R.id.rb_opD:
+                            selectedAns = rb_opD.getText().toString();
+                            break;
+
+                    }
+                    Log.i("print22", "" + correctAns);
+
+
+
+                    if (selectedAns.equals(correctAns)){
+
+                        rbselect.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.green));
+                        rbcorrect=rbselect;
+                        rb_opA.setClickable(false);
+                        rb_opB.setClickable(false);
+                        rb_opC.setClickable(false);
+                        rb_opD.setClickable(false);
+                        Log.i("selectedans",""+selectedAns);
+                        Log.i("correctans2",""+correctAns);
+                        correctValue++;
+
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                        editor.putInt(Value, correctValue);
+                        editor.commit();
+                        Toast.makeText(QuestionActivity.this,""+ correctValue,Toast.LENGTH_LONG).show();
+
 //                    rg_option.getCheckedRadioButtonId()
+
+                    }else {
+                        rbselect.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.red));
+                        if(rb_opA.getText().toString().equals(correctAns)){
+                            rb_opA.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.green));
+                            rbcorrect=rb_opA;
+                        }else  if(rb_opB.getText().toString().equals(correctAns)){
+                            rb_opB.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.green));
+                            rbcorrect=rb_opB;
+                        }else if(rb_opC.getText().toString().equals(correctAns)){
+                            rb_opC.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.green));
+                            rbcorrect=rb_opC;
+                        }else if(rb_opD.getText().toString().equals(correctAns)){
+                            rb_opD.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.green));
+                            rbcorrect=rb_opD;
+                        }
+
+                        rb_opA.setClickable(false);
+                        rb_opB.setClickable(false);
+                        rb_opC.setClickable(false);
+                        rb_opD.setClickable(false);
+
+
+
+                    }
+
+
                 }
-            }
-        });
+            });
+        }
 
         btn_sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv_ques.startAnimation(animFadein);
                 rg_option.startAnimation(animMove);
+
+                //rg_option.clearCheck();
+                checkedRadioButtonID =rg_option.getCheckedRadioButtonId();
+                Log.i("display",""+checkedRadioButtonID);
                 qno++;
+                Log.i("qno22",""+qno);
                 getQuestion(qno);
-                rg_option.clearCheck();
+                correctAns=null;
+                //correctAns = null;
+                Log.i("print23",""+correctAns);
+                rb_opA.setChecked(false);
+                rb_opB.setChecked(false);
+                rb_opC.setChecked(false);
+                rb_opD.setChecked(false);
+
+
                 btn_sub.setEnabled(false);
                 btn_sub.setBackgroundResource(R.color.centercolor);
             }
         });
 
+        bt_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuestionActivity.this,"Your Score is:"+ correctValue,Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
     private void getQuestion(int qno){
@@ -117,8 +206,8 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.getChildrenCount() !=0) {
+//                    totalques= dataSnapshot.getChildrenCount();
                     for (DataSnapshot child : dataSnapshot.getChildren()) {
-                        Log.i("getdata2", "" + dataSnapshot.toString());
                         QuestionModel qModel = child.getValue(QuestionModel.class);
 
                         tv_ques.setText(qModel.getQuestion());
@@ -129,7 +218,9 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
                         correctAns=qModel.getCorrect();
                     }
                 }else {
-                    btn_sub.setText("Submit");
+                    btn_sub.setVisibility(View.GONE);
+                    bt_done.setVisibility(View.VISIBLE);
+
                 }
             }
 
@@ -138,10 +229,27 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
 
             }
         });
+
     }
 
     @Override
     public void onAnimationStart(Animation animation) {
+
+        rb_opA.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.black));
+        rb_opB.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.black));
+        rb_opC.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.black));
+        rb_opD.setTextColor(ContextCompat.getColor(QuestionActivity.this, R.color.black));
+
+        rb_opA.setChecked(false);
+        rb_opB.setChecked(false);
+        rb_opC.setChecked(false);
+        rb_opD.setChecked(false);
+
+        rb_opA.setClickable(true);
+        rb_opB.setClickable(true);
+        rb_opC.setClickable(true);
+        rb_opD.setClickable(true);
+
 
     }
 
@@ -155,3 +263,4 @@ public class QuestionActivity extends AppCompatActivity implements Animation.Ani
 
     }
 }
+
