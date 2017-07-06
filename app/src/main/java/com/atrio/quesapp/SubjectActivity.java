b387cd8a510ba2ca57ea65e1a5abd9ff544307ce
arpita4462 <arpitapatel4462@gmail.com>
2017-07-06 12:36:34 +0530

    Activity Added

--- app/src/main/java/com/atrio/quesapp/SubjectActivity.java
+++ app/src/main/java/com/atrio/quesapp/SubjectActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.atrio.quesapp.Adapter.RecycleviewAdapter;
 import com.atrio.quesapp.model.ShowData;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
@@ -28,7 +31,6 @@ public class SubjectActivity extends AppCompatActivity {
     private GridLayoutManager lLayout;
     private FirebaseStorage storage;
     private StorageReference storageRef;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,16 +47,74 @@ public class SubjectActivity extends AppCompatActivity {
         final SpotsDialog dialog = new SpotsDialog(SubjectActivity.this,R.style.Custom);
         dialog.show();
 
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("Subject");
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        storage=FirebaseStorage.getInstance();
+        storageRef = storage.getReference("Subject");
         Query query_catlist = rootRef.orderByKey();
         query_catlist.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+//                    Log.i("subject56",dataSnapshot1.getKey());
+//                    showimg(getimg);
+                    final ShowData data =  new ShowData();
+//                    data.setImg(data.getImg());
+                    data.setSub(dataSnapshot1.getKey());
+                    Log.i("subject564",data.getSub());
 
-                    ShowData data =  dataSnapshot1.getValue(ShowData.class);
-                    data.setImg(data.getImg());
-                    data.setSub(data.getSub());
+                    storageRef.child(data.getSub()).getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+                        @Override
+                        public void onSuccess(StorageMetadata storageMetadata) {
+                            Log.i("imagename",""+storageMetadata.getPath());
+                            data.setImg(storageMetadata.getPath());
+
+
+                        }
+                    });
+
+/*
+                    storageRef.child(data.getSub()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Log.i("subject56",uri.toString());
+
+                            data.setImg(uri.toString());
+
+                            // Got the download URL for 'users/me/profile.png'
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle any errors
+                        }
+                    });
+*/
+/*
+                    try {
+
+                        localFile = File.createTempFile("images", "png");
+
+                        storageRef.child(data.getSub()).getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                                img_ques.setImageBitmap(bitmap);
+                                Log.i("storageimg",""+bitmap);
+
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+*/
+
+
                     arrayList.add(data);
 
                 }
