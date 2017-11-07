329a2c15917ea8a18d9b8daeb95ad3e19485b071
Arpita <arpitapatel4462@gmail.com>
2017-11-07 17:18:32 +0530

    fab dialog included

--- app/src/main/java/com/atrio/quesapp/custom/CustomFabDialog.java
+++ app/src/main/java/com/atrio/quesapp/custom/CustomFabDialog.java
@@ -1,21 +1,73 @@
 package com.atrio.quesapp.custom;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import com.atrio.quesapp.QuestionAnswerActivity;
+import com.atrio.quesapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Admin on 07-11-2017.
  */
 
-public class CustomFabDialog {
+public class CustomFabDialog extends Dialog{
+    Context mycontext;
+    Button btn_cancel,btn_go;
+    EditText et_quesno;
+    TextInputLayout inputLayoutName;
+    private ProgressBar progressBar;
 
+    public CustomFabDialog(Context context) {
+        super(context);
+        mycontext=context;
 
-    public CustomFabDialog(QuestionAnswerActivity questionAnswerActivity) {
-        
     }
 
-    public void requestWindowFeature(int featureNoTitle) {
-    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView();
+        setContentView(R.layout.custom_fab_dialog);
 
-    public void show() {
+        et_quesno = (EditText) findViewById(R.id.et_quesno);
+        btn_cancel = (Button) findViewById(R.id.btn_cancel);
+        btn_go = (Button) findViewById(R.id.btn_go);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        inputLayoutName = (TextInputLayout)findViewById(R.id.input_layout_frg_id);
+
+        btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quesno = et_quesno.getText().toString().trim();
+
+                if (TextUtils.isEmpty(quesno)) {
+                    inputLayoutName.setError(getContext().getString(R.string.enterquesnofield));
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        });
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
